354d21039eed5e90633dbd5e5e0da9587a326d67
trendimedia <163422456+trendimedia@users.noreply.github.com>
2026-09-11 08:44:50 +0000

    Preparing for sign up

---
+++ muddy/app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { SignupForm } from "@/components/signup-form"
+
+const SignUp = () => {
+    return (
+        <div className="">
+            <SignupForm />
+        </div>
+    )
+}
+
+export default SignUp
